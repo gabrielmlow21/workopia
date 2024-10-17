dc558484a2c37d9d61c25a92a968fc6346e0143e
Gabriel Low <lowgabriel213@gmail.com>
2024-10-17 17:26:52 +0800

    28: Generate resource routes and methods

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,28 +6,60 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    public function index() {
-        $title = "Available Jobs";
-        $jobs = [
-            "Web Developer",
-            "Database Admin",
-            "Software Engineer",
-            "Systems Analyst"
-        ];
-        return view('jobs.index', compact('title', 'jobs'));
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $jobs = ['Web Developer', 'Database Admin', 'Software Engineer', 'Systems Analyst'];
+        return view('jobs.index', compact('jobs'));
     }
 
-    public function create() {
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
         return view('jobs.create');
     }
 
-    public function show(string $id) {
-        return "Showing Job $id";
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function store(Request $request) {
-        $title = $request->input('title');
-        $description = $request->input('description');
-        return "Title: $title, Description $description";
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
