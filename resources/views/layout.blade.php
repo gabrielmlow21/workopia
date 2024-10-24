ebae0dbcc15f2657d0e2b3ac3a796482d0507a7e
Gabriel Low <lowgabriel213@gmail.com>
2024-10-24 22:59:06 +0800

    41: Mobile Menu Toggle

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-    />
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
-    <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
-    
+    <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
+
 </head>
+
 <body class="bg-gray-100">
     <x-header />
-    <main class="container mx-auto p-4 mt-4">
-        {{$slot}}
+    <main class="container mx-auto mt-4 p-4">
+        {{ $slot }}
     </main>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
+
 </html>
