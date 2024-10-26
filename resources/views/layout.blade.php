ba5de5ed1eceff0b798692aa8dc0edeb8608c34c
Gabriel Low <lowgabriel213@gmail.com>
2024-10-27 01:42:45 +0800

    42: Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,13 +8,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     @vite('resources/css/app.css')
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
-
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto mt-4 p-4">
         {{ $slot }}
     </main>
